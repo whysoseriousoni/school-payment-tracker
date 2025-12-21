c1b67263d8934be7128c03b79a8648c332c2854d
Pradeep-Elumalai <sweetmolu9@gmail.com>
2025-12-21 19:18:59 +0530

    [PE] Started billing details

--- src/helper/utils.py
+++ src/helper/utils.py
@@ -1,6 +1,9 @@
 from typing import Any, Optional
 
 def check_null(value: Any):
+    if isinstance(value, list):
+        if len(value)==0 or value is None:
+            return False
     if isinstance(value, str):
         if value=="" or value is None:
             return False
@@ -11,3 +14,11 @@ def get_or_default(dictionary, key, default=""):
     if not check_null(value_retrieved):
         return default
     return value_retrieved
+
+def get_index_or_default(options: list, search_for, default=0):
+    if check_null(options):
+        return default
+    try:
+        return options.index(value=search_for)
+    except IndexError:
+        return default
--- src/ui/billing/billing.py
+++ src/ui/billing/billing.py
@@ -1,14 +1,62 @@
 import streamlit as st
 
-from helper.utils import get_or_default
+from helper.utils import get_index_or_default, get_or_default
 from ui.login import get_user_details
 
 st.markdown("# Billing Section")
 
-is_authenticated = get_or_default(dictionary=st.session_state, key="LOGGED_IN", default=False)
+is_authenticated = get_or_default(
+    dictionary=st.session_state, key="LOGGED_IN", default=False
+)
 is_admin = get_or_default(dictionary=st.session_state, key="ADMIN_FLAG", default=False)
 whoami = get_or_default(dictionary=st.session_state, key="USER_TYPE", default=False)
 
 if not is_authenticated:
     get_user_details()
     st.stop()
+
+## ----- STUDENT SELECTION ----- ##
+
+st.markdown("### Select Student Information")
+st.text_input(label="Enter Student ID", key="STUDENT_ID")
+st.text_input(label="Enter Student Name", key="STUDENT_NAME")
+
+
+## ----- BILLING SELECTION ----- ##
+
+BILLING_TYPES = [
+    "Term Fee",
+    "Van Fee",
+    "Exam Fee",
+    "Computer Fee",
+    "Uniform Fee",
+    "Book Fee",
+    "ID Fee",
+    "Lunch Fee",
+    "Petrol Fee",
+]
+# "Custom Fee",
+
+billing_type = get_or_default(
+    dictionary=st.session_state, key="BILLING_TYPE", default=None
+)
+CUSTOM_BILLING_TYPE = "Custom Fee"
+
+st.selectbox(
+    options=[*BILLING_TYPES, CUSTOM_BILLING_TYPE],
+    key="BILLING_TYPE",
+    label="Choose Billing Type",
+    index=get_index_or_default(
+        options=BILLING_TYPES, search_for=billing_type, default=0
+    ),
+)
+
+if billing_type == CUSTOM_BILLING_TYPE:
+    # Overwrite
+    st.text_input(label="Enter Billing Type", key="CUSTOM_BILL_TYPE")
+    billing_type = get_or_default(
+        dictionary=st.session_state, key="CUSTOM_BILL_TYPE", default=""
+    )
+
+if st.button(label="Create Bill"):
+    st.toast("Bill Initiated")
